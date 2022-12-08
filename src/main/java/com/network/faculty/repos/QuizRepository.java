0fc26123d535622475c256daae20f35191a4b6ce
Ihor Fedorchenko <invisiblepanda04@gmail.com>
2022-12-08 10:48:24 +0200

    Quiz repo

--- src/main/java/com/network/faculty/repos/QuizRepository.java
+++ src/main/java/com/network/faculty/repos/QuizRepository.java
@@ -1,2 +1,12 @@
-package com.network.faculty.repos;public interface QuizRepository {
+package com.network.faculty.repos;
+
+import com.network.faculty.entities.Quiz;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Transactional
+public interface QuizRepository extends JpaRepository<Quiz, Long> {
+    List<Quiz> getQuizzesBySenderId(Long senderId);
 }
