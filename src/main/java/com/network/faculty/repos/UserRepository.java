c7836b6ebd287b3e944d74023b073e076fcdcf03
Ihor Fedorchenko <invisiblepanda04@gmail.com>
2022-12-13 12:06:07 +0200

    user repo plus

--- src/main/java/com/network/faculty/repos/UserRepository.java
+++ src/main/java/com/network/faculty/repos/UserRepository.java
@@ -2,10 +2,20 @@ package com.network.faculty.repos;
 
 import com.network.faculty.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 import javax.transaction.Transactional;
 
 @Transactional
 public interface UserRepository extends JpaRepository<User, Long> {
     User getUserByEmail(String email);
+
+    @Modifying
+    @Query("UPDATE User user SET user.blocked = 1 WHERE user.id = ?1")
+    void blockById(Long id);
+
+    @Modifying
+    @Query("UPDATE User user SET user.blocked = 0 WHERE user.id = ?1")
+    void unblockById(Long id);
 }
